69c83c0ebb1a224d5821b0046db35e5b8cc3ccde
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 17:08:05 +0530

    print the bot name and birth year

--- src/chatbot/Bot.java
+++ src/chatbot/Bot.java
@@ -3,7 +3,9 @@ package chatbot;
 public class Bot {
 
     public void greet(String assistantName, String birthYear) {
-
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
     }
 
 }
