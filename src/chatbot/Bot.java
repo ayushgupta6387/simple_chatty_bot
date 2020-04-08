cb3bf37bf3341fc1ccf90547d3dc34e58f08cb08
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 17:10:07 +0530

    created method to take the name of the user

--- src/chatbot/Bot.java
+++ src/chatbot/Bot.java
@@ -1,6 +1,9 @@
 package chatbot;
 
+import java.util.Scanner;
+
 public class Bot {
+    Scanner scanner = new Scanner(System.in);
 
     public void greet(String assistantName, String birthYear) {
         System.out.println("Hello! My name is " + assistantName + ".");
@@ -8,4 +11,8 @@ public class Bot {
         System.out.println("Please, remind me your name.");
     }
 
+    public void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
 }
