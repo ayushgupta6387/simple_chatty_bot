aece3fae2495a8ad1b318254a468a2dfc253f9f7
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-04-08 17:15:10 +0530

    find out the age using remainders

--- src/chatbot/Bot.java
+++ src/chatbot/Bot.java
@@ -22,6 +22,8 @@ public class Bot {
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
 
     }
 
